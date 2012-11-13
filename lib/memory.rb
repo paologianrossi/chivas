460ffa53329d569deb67c98088abb89743008f37
Paolo Gianrossi <paolino.gianrossi@gmail.com>
2012-11-13 11:29:59 +0100

    document Memory class

--- lib/memory.rb
+++ lib/memory.rb
@@ -1,23 +1,31 @@
 class Memory
 
+  # the number of words the instance of Memory can hold
   attr_accessor :size
-  
-  def initialize(options = {})
-    @size = options[:size] or raise "Size is a required parameter to Memory"
+
+  # create a new instance of Memory with given +size+
+  def initialize(size)
+    @size = size or raise "Size is a required parameter to Memory"
     @storage = []
   end
 
+  # store a value in the Memory instance at address +address+. The
+  # value is what is returned by the yielded block, and should be an
+  # integer value, while +address+ should be between 0 and +size+
   def store(address)
     check_address(address)
     value = Integer(yield(address))
     @storage[address]=value
   end
 
+  # returns the value stored in the Memory instance at address
+  # +address+, or +nil+ if no value was stored at said address.
   def read(address)
     check_address(address)
     @storage[address]
   end
 
+  # resets all cells to +nil+, as if the instance was power cycled.
   def reset
     @storage = []
   end
